5058e4c4a32b13f8e5da2d0d8d4c361c160570be
Pooshin <0xshinpoo@gmail.com>
2022-10-19 18:35:52 +0200

    reverse

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <h1 className="mt-24 mb-12 font-bold text-3xl">Latest Posts</h1>
-      {posts.map((post,id) => (
+      {posts.reverse().map((post,id) => (
         <PostCard key={id}
           title={post.data.title}
           publishedOn={post.data.publishedOn}
